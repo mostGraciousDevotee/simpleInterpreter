28d1f9e6904c5cc20e9ae2778817e16df5e52bba
mostGraciousDevotee <112270626+mostGraciousDevotee@users.noreply.github.com>
2022-09-06 06:52:56 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define MAXWORD 100
-#incule "lexer.h"
+#include "cli.h"
 
-char *junkWordTab[] =
+#define MAXCMD_ARG 64
+
+int main(void)
 {
-  "A",
-  "The", "This",
-  "We",
-};
-
+  printf("Gabrielix Intepreter\nWritten by mostGraciousDevotee.\n\n");
+  
+  char *cmd_argv[MAXCMD_ARG];
+  int cmd_argc;
+  
+  while((cmd_argc = getcmd(cmd_argv, MAXCMD_ARG)) >= 0)
+  {
+    if (strcmp(cmd_argv[0], "echo") == 0)
+    {
+      echo(cmd_argc, cmd_argv);
+    }
+    
+    else if (strcmp(cmd_argv[0], "exit") == 0)
+    {
+      break;
+    }
+    
+    else if (strcmp(cmd_argv[0], "help") == 0)
+    {
+      help(cmd_argc, cmd_argv);
+    }
+    
+    else if (strcmp(cmd_argv[0], "wcount") == 0)
+    {
+      wcount(cmd_argc, cmd_argv);
+    }
+    
+    else
+    {
+      printf("Command not found!\n\n");
+    }
+    
+    afree(cmd_argv[0]);
+  }
+  
+}
