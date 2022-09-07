5bed0772adb9ab97ba35c58498dec4739d02de06
mostGraciousDevotee <112270626+mostGraciousDevotee@users.noreply.github.com>
2022-09-07 12:04:53 +0700

    Create alloc.c

---
+++ alloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define ALLOCSSIZE 1024
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n)
+{
+  if (allocbuf + ALLOCSIZE - allocp >= n)
+  {
+    allocp += n;
+    return allocp - n;
+  }
+  
+  else
+  {
+    return 0;
+  }
+}
+
+void afree(char *p)
+  if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+  {
+    allocp = p;
+  }
